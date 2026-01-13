36823890a9b58ff1811b6bd8d2ae352ed9b6cf51
root <root@airflow-dev.us-west1-b.c.verdant-inquiry-484111-t8.internal>
2026-01-13 16:12:42 +0000

    chore: configure email notifications for job failures

--- dags/seoul_citydata_dag.py
+++ dags/seoul_citydata_dag.py
@@ -41,7 +41,8 @@ def run_collection_job():
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'email_on_failure': False,
+    'email_on_failure': True,
+    'email': ['ansemr99@gmail.com'],
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
